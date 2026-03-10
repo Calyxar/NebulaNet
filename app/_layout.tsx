9554487bc580039d977680c8dc133a9c6b8c0ef5
Your Name <alyssajbaker04@gmail.com>
2026-03-10 17:02:11 -0500

    Fixes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -76,7 +76,7 @@ export default function RootLayout() {
                   <Stack.Screen name="(auth)" />
                   <Stack.Screen name="(tabs)" />
                   <Stack.Screen name="profile" />
-                  <Stack.Screen name="boost/[postId]" />
+                  <Stack.Screen name="boost" />
                   <Stack.Screen name="settings" />
                   <Stack.Screen name="+not-found" />
                 </Stack>
