4da18ae15943226ab329c6f460eb18746a1ac985
Your Name <alyssajbaker04@gmail.com>
2026-03-25 19:23:07 -0500

    phone login/signup with OTP verification

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -10,7 +10,7 @@ import { getFirestore } from "firebase/firestore";
 import { getFunctions } from "firebase/functions";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY!,
   authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN!,
   projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID!,
