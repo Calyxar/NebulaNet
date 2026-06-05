557ad945b2ee1a24ae3561abf6c4723e465372a0
Your Name <alyssajbaker04@gmail.com>
2026-06-05 05:11:29 -0500

    fix: hide all stack headers globally

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -141,7 +141,7 @@ function RootLayout() {
       <ThemeSync />
       <PushNotificationSetup />
 
-      <Stack>
+      <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="index" options={{ headerShown: false }} />
